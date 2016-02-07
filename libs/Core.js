3cfe9e4f225b04a9a3fb29dee011ea7bccca4789
Michael Perrotte <mike@mikecorp.ca>
2016-02-07 02:14:57 -0500

    added `next` callback for system control flow

--- libs/Core.js
+++ libs/Core.js
@@ -26,31 +26,38 @@ class CollinsMumble extends Emitter.EventEmitter {
     this.Runtime['hello'] = 'world';
   }
 
-  init() {
+  init(next) {
     async.series([
       Loader.init.bind(this), // TODO: remove this?
       Loader.initConfig.bind(this),
       Loader.initCogs.bind(this),
       Loader.initActions.bind(this)
     ], (err, result) => {
+      this.initialized = true;
 
-      // INFO: all the initializations have been complete
+      // INFO: all the initializations have been completed
+      next(err);
     });
   }
 
   log() {}
 
-  connect() {
-    const mumbleCB = (error, client) => {
-      if (error) { throw new CoreError('ConnectError', error); }
-      this._client = client;
-      client.authenticate(this.config.username, this.config.password);
-      client.on('initialized', Listeners.onInit.bind(this));
-      client.on('error', (data) => { console.log('error', data); });
-      client.on('disconnect', Listeners.onDisconn.bind(this));
-      client.on('ready', Listeners.onReady.bind(this));
-      client.on('message', _.bind(Listeners.onMessage, this, _, _, _, client));
-      client.on('user-connect', Listeners.onUserConn.bind(this));
+  connect(next) {
+    const mumbleCB = (err, client) => {
+      if (err) {
+        let error = new CoreError('ConnectError', err);
+        next(error);
+      } else {
+        this._client = client;
+        client.authenticate(this.config.username, this.config.password);
+        client.on('initialized', Listeners.onInit.bind(this));
+        client.on('error', (data) => { console.log('error', data); });
+        client.on('disconnect', Listeners.onDisconn.bind(this));
+        client.on('ready', Listeners.onReady.bind(this));
+        client.on('message', _.bind(Listeners.onMessage, this, _, _, _, client));
+        client.on('user-connect', Listeners.onUserConn.bind(this));
+        next(null);
+      }
     };
 
     mumble.connect(this.config.server, this.config.ssl, mumbleCB.bind(this));
