11c060e90889ee8fb247d05a8a40b8bf13c0830d
Michael Perrotte <mike@mikecorp.ca>
2016-02-11 07:16:52 -0500

    cleaned up code

--- libs/Listeners.js
+++ libs/Listeners.js
@@ -1,7 +1,10 @@
 'use strict';
 
 class Listeners {
-  static onMessage(message, user, scope, client) {}
+  static onMessage(message, user, scope) {
+    let args = Array.prototype.slice.apply(arguments);
+    console.log('>>', 'CollinsMumble', 'onMsg', args);
+  }
 
   static onUserConn(user) {}
 
